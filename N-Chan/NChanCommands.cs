5d2128c1671f8d29cb3cf128f3d320ad16406c24
Rui Silva <rfilipecs@outlook.pt>
2025-01-14 23:58:30 +0000

    Much better (but still bad) understanding of service providers and Discord.Net architecture implemented

--- N-Chan/NChanCommands.cs
+++ N-Chan/NChanCommands.cs
@@ -1,9 +1,8 @@
 namespace GigaSharp;
 
 using Discord.Interactions;
-using Discord.WebSocket;
 
-public class NChanCommands : InteractionModuleBase<SocketInteractionContext<SocketInteraction>>
+public class NChanCommands : InteractionModuleBase<SocketInteractionContext>
 {
     [SlashCommand("echo-ping", "Receive a pong, followed by an echo of the input")]
     public async Task EchoPing(string input){
--- N-Chan/NChanMain.cs
+++ N-Chan/NChanMain.cs
@@ -1,18 +1,25 @@
 namespace GigaSharp;
 
+using System.Reflection;
 using Discord;
 using Discord.Interactions;
 using Discord.WebSocket;
 
 public class NChanMain
 {
+    private static IServiceProvider? services;
     /*
         Set up and start the actual N-Chan bot and keep it running indefinitely.
     */
     public static async Task StartBot(){
-        //Create the DiscordSocketClient and hook our logging method to the log event
-        DiscordSocketClient client = new DiscordSocketClient();
+        //Create the collection of services required to perform the bot's functions.
+        services = new ServiceCollection()
+            .AddSingleton<DiscordSocketClient>()
+            .AddSingleton(x => new InteractionService(x.GetRequiredService<DiscordSocketClient>().Rest))
+            .BuildServiceProvider();
+        DiscordSocketClient client = services.GetRequiredService<DiscordSocketClient>();
         client.Log += Log;
+        services.GetRequiredService<InteractionService>().Log += Log;
 
         //Perform the login (currently logging into the test bot, not nchan)
         //await client.LoginAsync(TokenType.Bot, Environment.GetEnvironmentVariable("NCHAN_TOKEN"));
@@ -20,7 +27,8 @@ public class NChanMain
         await client.StartAsync();
 
         //Create interaction framework, load interaction module handler, and hook intraction processing to interaction event
-        _ = InitCommands(client);
+        //From this point on, there is no need to work in a static environment, and for safety purposes, we won't.
+        _ = new NChanMain().SetUp();
         
         // Run task indefinitely.
         await Task.Delay(-1);
@@ -29,21 +37,32 @@ public class NChanMain
     /*
         Initialize all commands. Incomplete for now.
     */
-    private static async Task InitCommands(DiscordSocketClient client){
-        Console.WriteLine("REGISTERING COMMANDS...");
-        InteractionService service = new InteractionService(client.Rest);
+    private async Task SetUp(){
+        if(services == null){
+            throw new Exception("Services undefined upon attempting to set up n-chan");
+        }
+        Console.WriteLine("PERFORMING N-CHAN SETUP");
+        InteractionService service = services.GetRequiredService<InteractionService>();
+        DiscordSocketClient client = services.GetRequiredService<DiscordSocketClient>();
+        client.Ready += ReadyAsync;
         Console.WriteLine("ADDING MODULE...");
-        await service.AddModuleAsync<NChanCommands>(null);
+        await service.AddModulesAsync(Assembly.GetEntryAssembly(), services);
         Console.WriteLine("MODULE ADDED!");
         client.InteractionCreated += async (interaction) =>
         {
             SocketInteractionContext<SocketInteraction> ctx = new SocketInteractionContext<SocketInteraction>(client, interaction);
-            await service.ExecuteCommandAsync(ctx, null);
+            await service.ExecuteCommandAsync(ctx, services);
         };
+    }
+
+    private async Task ReadyAsync(){
+        if(services == null){
+            throw new Exception("Services undefined upon attempting to register n-chan commands");
+        }
         Console.WriteLine("STARTING REGISTRY...");
-        await service.RegisterCommandsToGuildAsync(1181165855522959420);
+        await services.GetRequiredService<InteractionService>().RegisterCommandsToGuildAsync(1181165855522959420);
         Console.WriteLine("COMMANDS REGISTERED!");
-    } 
+    }
 
     private static Task Log(LogMessage msg)
     {
