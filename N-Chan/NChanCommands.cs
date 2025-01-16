84d47aaae6ef733eb90ffadb69c43bfccb224ebf
Rui Silva <rfilipecs@outlook.pt>
2025-01-16 12:15:59 +0000

    Webscraping functional, attempting to remove hidden spans from analysis

--- N-Chan/NChanCommands.cs
+++ N-Chan/NChanCommands.cs
@@ -11,14 +11,20 @@ public class NChanCommands : InteractionModuleBase<SocketInteractionContext>
 
     [SlashCommand("book", "Get information about a particular book!")]
     public async Task Book(int id){
-        Book book = WebScraping.GetBookFromWeb(id);
-        if(book != null){
-            await RespondAsync("BOOK RECEIVED!!!\nName: "+book.Name+
-            "\nFirst tag: "+book.Tags[0]+"\nLast tag: "+book.Tags[book.Tags.Length-1]+
-            "\nFirst character: "+book.Characters[0]+"\nLast character: "+book.Characters[book.Characters.Length-1]+
-            "\nPages: "+book.Pages+"\nParody: "+book.Parody[0]);
-        }else{
-            await RespondAsync("I'm sowwy! I couldn't find that book, master :(");
+        try{
+            Book book = WebScraping.GetBookFromWeb(id);
+            if(book != null){
+                await RespondAsync("BOOK RECEIVED!!!\nName: "+book.Name+
+                "\nFirst tag: "+book.Tags[0]+"\nLast tag: "+book.Tags[book.Tags.Length-1]+
+                "\nFirst character: "+book.Characters[0]+"\nLast character: "+book.Characters[book.Characters.Length-1]+
+                "\nPages: "+book.Pages+"\nParody: "+book.Parody[0]);
+            }else{
+                await RespondAsync("I'm sowwy! I couldn't find that book, master :(");
+            }
+        }catch(Exception e){
+            Console.WriteLine(e.Message);
+            Console.WriteLine(e.StackTrace);
+            await RespondAsync("I'm sowwy! There was an error processing that book, master :(");
         }
     }
 }
--- N-Chan/WebScraping.cs
+++ N-Chan/WebScraping.cs
@@ -4,66 +4,54 @@ namespace GigaSharp;
 
 public class WebScraping{
     public static Book GetBookFromWeb(int id){
-        try{
-            HtmlDocument doc = new HtmlWeb().Load("https://nhentai.xxx/g/"+id);
-            if(doc == null) { return null; }
+        HtmlDocument doc = new HtmlWeb().Load("https://nhentai.xxx/g/"+id);
+        if(doc == null) { return null; }
 
-            Console.WriteLine("Found html doc.");
-            HtmlNode infoBlock = doc.DocumentNode.SelectSingleNode("//div[@class=\"info\"]");
-            Book.BookBuilder builder = new Book.BookBuilder().AddId(id)
-                .AddDoc(doc)
-                .AddName(infoBlock.Element("h1").InnerText)
-                .AddCover(doc.DocumentNode.SelectSingleNode("//div[@class=\"cover\"]/a/img").Attributes["data-src"].Value);
-            Console.WriteLine("Name: "+infoBlock.Element("h1").InnerText);
-            
-            HtmlNodeCollection infoBlockCategories = infoBlock.SelectNodes("//li[position()<last()-1]/span");
-            foreach(HtmlNode node in infoBlockCategories){
-                Console.WriteLine("Processing category "+node.InnerText);
-                string[] elements = new string[node.ParentNode.ChildNodes.Count-1];
-                fillInfoBlockArray(elements, node);
-                switch (node.InnerText){
-                    case "Parodies:": builder.AddParody(elements);
-                        break;
-                    case "Characters:": builder.AddCharacters(elements);
-                        break;
-                    case "Tags:": builder.AddTags(elements);
-                        break;
-                    case "Artists:": builder.AddArtists(elements);
-                        break;
-                    case "Groups:": builder.AddGroups(elements);
-                        break;
-                    case "Languages:": builder.AddLanguages(elements);
-                        break;
-                    case "Categories:": builder.AddCategories(elements);
-                        break;
-                }
+        HtmlNode infoBlock = doc.DocumentNode.SelectSingleNode("//div[@class=\"info\"]");
+        Book.BookBuilder builder = new Book.BookBuilder().AddId(id)
+            .AddDoc(doc)
+            .AddName(infoBlock.Element("h1").InnerText)
+            .AddCover(doc.DocumentNode.SelectSingleNode("//div[@class=\"cover\"]/a/img").Attributes["data-src"].Value);
+        Console.WriteLine("Name: "+infoBlock.Element("h1").InnerText);
+        
+        HtmlNodeCollection infoBlockCategories = infoBlock.SelectNodes("//li[position()<last()-1]/span");
+        foreach(HtmlNode node in infoBlockCategories){
+            string[] elements = new string[node.ParentNode.ChildNodes.Count-1];
+            fillInfoBlockArray(elements, node);
+            switch (node.InnerText){
+                case "Parodies:": builder.AddParody(elements);
+                    break;
+                case "Characters:": builder.AddCharacters(elements);
+                    break;
+                case "Tags:": builder.AddTags(elements);
+                    break;
+                case "Artists:": builder.AddArtists(elements);
+                    break;
+                case "Groups:": builder.AddGroups(elements);
+                    break;
+                case "Languages:": builder.AddLanguages(elements);
+                    break;
+                case "Categories:": builder.AddCategories(elements);
+                    break;
             }
-            builder.AddPages(int.Parse(infoBlock.SelectSingleNode("//span[@class=\"tag_name pages\"]").InnerText));
-
-            return builder.Build();
-        }catch(Exception e){
-            Console.WriteLine("Exception thrown somewhere: "+e.Message);
-            Console.WriteLine(e.StackTrace);
-            return null;
         }
+        builder.AddPages(int.Parse(infoBlock.SelectSingleNode("//span[@class=\"tag_name pages\"]").InnerText));
+
+        return builder.Build();
     }
 
     private static void fillInfoBlockArray(string[] infoBlockCategory, HtmlNode htmlReference){
         htmlReference = htmlReference.NextSibling;
         int i = 0;
-        Console.WriteLine("NextSibling of span is "+htmlReference.OuterHtml);
-        Console.WriteLine("Attempting to fill array...");
         for(i = 0; htmlReference!=null && i < infoBlockCategory.Length; i++){
             HtmlNode name = htmlReference.SelectSingleNode("span[@class=\"tag_name\"]");
-            /*IEnumerable<HtmlNode> discard = name.ChildNodes.Where(x => x.NodeType == HtmlNodeType.Element);
+            HtmlNodeCollection discard = name.SelectNodes("span");
             foreach(HtmlNode child in discard){
                 name.RemoveChild(child);
-            }*/
-            Console.WriteLine("Block "+name.InnerText+" found.");
+            }
             infoBlockCategory[i] = name.InnerText;
             htmlReference = htmlReference.NextSibling;
         }
-        Console.WriteLine("Array filled.");
         if(htmlReference != null || i < infoBlockCategory.Length){
             throw new Exception("Error on filling an info block array! Index at "+i+" and htmlreference null: "+(htmlReference==null));
         }
