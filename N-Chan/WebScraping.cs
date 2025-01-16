c610549a90a03c7385320d5f52501a113586070e
Rui Silva <rfilipecs@outlook.pt>
2025-01-16 10:44:17 +0000

    Debugging prints added thorughout webscraping code

--- N-Chan/WebScraping.cs
+++ N-Chan/WebScraping.cs
@@ -8,14 +8,17 @@ public class WebScraping{
             HtmlDocument doc = new HtmlWeb().Load("https://nhentai.xxx/g/"+id);
             if(doc == null) { return null; }
 
+            Console.WriteLine("Found html doc.");
             HtmlNode infoBlock = doc.DocumentNode.SelectSingleNode("//div[@class=\"info\"]");
             Book.BookBuilder builder = new Book.BookBuilder().AddId(id)
                 .AddDoc(doc)
                 .AddName(infoBlock.Element("h1").InnerText)
                 .AddCover(doc.DocumentNode.SelectSingleNode("//div[@class=\"cover\"]/a/img").Attributes["data-src"].Value);
+            Console.WriteLine("Name: "+infoBlock.Element("h1").InnerText);
             
             HtmlNodeCollection infoBlockCategories = infoBlock.SelectNodes("li/span");
             foreach(HtmlNode node in infoBlockCategories){
+                Console.WriteLine("Processing category "+node.InnerText);
                 string[] elements = new string[node.ParentNode.ChildNodes.Count-1];
                 fillInfoBlockArray(elements, node);
                 switch (node.InnerText){
@@ -39,6 +42,7 @@ public class WebScraping{
 
             return builder.Build();
         }catch{
+            Console.WriteLine("Exception thrown somewhere.");
             return null;
         }
     }
@@ -46,15 +50,18 @@ public class WebScraping{
     private static void fillInfoBlockArray(string[] infoBlockCategory, HtmlNode htmlReference){
         htmlReference = htmlReference.NextSibling;
         int i = 0;
+        Console.WriteLine("Attempting to fill array...");
         for(i = 0; htmlReference!=null && i < infoBlockCategory.Length; i++){
             HtmlNode name = htmlReference.SelectSingleNode("/span[@class=\"tag_name\"]");
             /*IEnumerable<HtmlNode> discard = name.ChildNodes.Where(x => x.NodeType == HtmlNodeType.Element);
             foreach(HtmlNode child in discard){
                 name.RemoveChild(child);
             }*/
+            Console.WriteLine("Block "+name.InnerText+" found.");
             infoBlockCategory[i] = name.InnerText;
             htmlReference = htmlReference.NextSibling;
         }
+        Console.WriteLine("Array filled.");
         if(htmlReference != null || i < infoBlockCategory.Length){
             Console.WriteLine("Index at "+i+" and htmlreference null: "+(htmlReference==null));
             //throw new Exception("Something has gone wrong.");
